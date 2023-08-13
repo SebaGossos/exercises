9d01216b8359d3934cad0c070e9e4f018ac352ac
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-08-13 14:59:57 -0300

    feat: resolve until exercise_23

--- ejercicios/ejercicio_23/ejercicio_23.ts
+++ ejercicios/ejercicio_23/ejercicio_23.ts
@@ -1,4 +1,3 @@
-"use strict";
 /* Enunciado Ejercicio 23:
 Crea una función que a la cual le pase unos números en un array y me decodifique
 el mensaje teniendo en cuenta que cada letra tiene un numero asignado.
@@ -9,22 +8,32 @@ por ejemplo la A es el numero 1 y la Z es el numero 26
 Ejemplos:
 decodificarMensaje([8, 15, 12, 1]);
  
-Devuelve:
+Devuelve: 
 HOLA */
+
+
+
 //!SOLUCION 1
 // function decodificarMensaje( message: number[] ): string {
 //     if ( !message ) throw new Error( 'Must send an argument message')
+
 //     let finishMessage = '';
+
 //     for ( const number of message ) finishMessage += String.fromCharCode( number + 64 )
+
 //     return finishMessage;
 // }
+
 //!SOLUCION 2
-function decodificarMensaje(message) {
-    if (!message)
-        throw new Error('Must send an argument message');
-    const numberMessage = message.map(num => {
+function decodificarMensaje( message: number[] ): string {
+    if ( !message ) throw new Error( 'Must send an argument message')
+
+    const numberMessage = message.map( num => {
         return num + 64;
-    });
-    return String.fromCharCode(...numberMessage);
+    })
+
+    return String.fromCharCode( ...numberMessage )
 }
-console.log(decodificarMensaje([8, 15, 12, 1]));
+
+
+console.log(decodificarMensaje( [8, 15, 12, 1] ))
